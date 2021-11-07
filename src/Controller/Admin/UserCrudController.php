b5d9821c75d543e5c9acd787060d224bdf4cc142
abbassiraja <abbassirajap4@gmail.com>
2021-11-07 12:48:31 +0100

    creation d'un dashboard admin

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            
+            ImageField::new('image')
+               ->setBasePath('images/')
+               ->setUploadDir('public/images')
+               ->setUploadedFileNamePattern('[randomhash].[extension]'),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+           
+            EmailField::new('email'),
+            TextField::new('password'),
+            
+        ];
+    }
+    
+}
